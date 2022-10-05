075da0cde2ee62b8addafc8d6738a08e83d86f4d
Alek30k <alemank23k@gmail.com>
2022-10-05 15:29:25 -0300

    last commit

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -2,7 +2,7 @@ import useFetch from "../../hooks/useFetch";
 import "./featuredProperties.css";
 
 const FeaturedProperties = () => {
-  const { data, loading, error } = useFetch("hotels?featured=true&limit=4");
+  const { data, loading, error } = useFetch("/hotels?featured=true&limit=4");
 
   return (
     <div className="fp">
@@ -10,7 +10,7 @@ const FeaturedProperties = () => {
         "Loading"
       ) : (
         <>
-          {data.map((item) => (
+          {/* {data.map((item) => (
             <div className="fpItem" key={item._id}>
               <img src={item.photos[0]} alt="" className="fpImg" />
               <span className="fpName">{item.name}</span>
@@ -25,7 +25,7 @@ const FeaturedProperties = () => {
                 </div>
               )}
             </div>
-          ))}
+          ))} */}
         </>
       )}
     </div>
