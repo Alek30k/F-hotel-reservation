36e44b5bf6c0744a308422a11c012a7d81185b94
Alek30k <alemank23k@gmail.com>
2022-10-07 14:15:47 -0300

    last commit

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -4,32 +4,27 @@ import "./featuredProperties.css";
 const FeaturedProperties = () => {
   const { data, loading } = useFetch("hotels?featured=true&limit=4");
   console.log(data);
+
+  if (loading) return <h2>Loading...</h2>;
+  if (!data) return <h2>No hay hoteles</h2>;
   return (
     <div className="fp">
-      {loading ? (
-        "Loading"
-      ) : (
-        <>
-          {data?.map((item) => {
-            return (
-              <div className="fpItem" key={item._id}>
-                <img src={item.photos[0]} alt="Photo" className="fpImg" />
-                <span className="fpName">{item.name}</span>
-                <span className="fpCity">{item.city}</span>
-                <span className="fpPrice">
-                  Starting from ${item.cheapestPrice}
-                </span>
-                {item.rating && (
-                  <div className="fpRating">
-                    <button>{item.rating}</button>
-                    <span>Excellent</span>
-                  </div>
-                )}
+      {data?.map((item) => {
+        return (
+          <div className="fpItem" key={item._id}>
+            <img src={item.photos[0]} alt="Photo" className="fpImg" />
+            <span className="fpName">{item.name}</span>
+            <span className="fpCity">{item.city}</span>
+            <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
+            {item.rating && (
+              <div className="fpRating">
+                <button>{item.rating}</button>
+                <span>Excellent</span>
               </div>
-            );
-          })}
-        </>
-      )}
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 };
