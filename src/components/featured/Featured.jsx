c50e9e2d21d2adb442e50064e24c78c0bd634dbb
Alek30k <alemank23k@gmail.com>
2022-10-08 10:48:11 -0300

    last comit

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -3,7 +3,7 @@ import "./featured.css";
 
 const Featured = () => {
   const { data, loading } = useFetch(
-    "https://b-hotel-reservation.herokuapp.com/api/hotels/countByCity?cities=berlin,madrid,london"
+    `${process.env.HEROKU}/hotels/countByCity?cities=berlin,madrid,london`
   );
 
   return (
