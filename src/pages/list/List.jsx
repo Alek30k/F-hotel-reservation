0632f99b20e2a5cf5197659f428a4f6adda56a1c
Alek30k <alemank23k@gmail.com>
2022-10-08 16:10:37 -0300

    last commit

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -18,7 +18,9 @@ const List = () => {
   const [max, setMax] = useState(undefined);
 
   const { data, loading, error, reFetch } = useFetch(
-    `/hotels?city=${destination}&min=${min || 0}&max=${max || 999}`
+    `https://b-hotel-reservation.herokupp.com/api/hotels?city=${destination}&min=${
+      min || 0
+    }&max=${max || 999}`
   );
 
   const handleClick = () => {
